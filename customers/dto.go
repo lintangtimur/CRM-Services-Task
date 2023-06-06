6e915cdf457923bef12559d913ae51264d71a448
lintangtimur <lintangtimur915@gmail.com>
2023-06-07 02:12:48 +0700

    Fixed: Get all customers mendapatkan data dari JSON eksternal dan simpan dalam database

--- customers/dto.go
+++ customers/dto.go
@@ -23,3 +23,23 @@ type DeleteCustomerResponse struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
 }
+type Response struct {
+	Page       int         `json:"page"`
+	PerPage    int         `json:"per_page"`
+	Total      int         `json:"total"`
+	TotalPages int         `json:"total_pages"`
+	Data       []UserData  `json:"data"`
+	Support    SupportInfo `json:"support"`
+}
+
+type UserData struct {
+	Email     string `json:"email"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Avatar    string `json:"avatar"`
+}
+
+type SupportInfo struct {
+	URL  string `json:"url"`
+	Text string `json:"text"`
+}
--- customers/repository.go
+++ customers/repository.go
@@ -1,7 +1,10 @@
 package customers
 
 import (
+	"encoding/json"
 	"gorm.io/gorm"
+	"io/ioutil"
+	"net/http"
 	"strconv"
 )
 
@@ -22,6 +25,7 @@ func (r Repository) DeleteCustomer(c *Customer, d *DeleteCustomerRequest) error 
 }
 
 func (r Repository) FindAllCustomers(firstname string, email string, limit string, page string) ([]Customer, error) {
+	parseDataAndSave("https://reqres.in/api/users?page=2", r.db)
 	var customer []Customer
 	limits, _ := strconv.Atoi(limit)
 	pages, _ := strconv.Atoi(page)
@@ -42,7 +46,47 @@ func (r Repository) FindAllCustomers(firstname string, email string, limit strin
 
 	return customer, err
 }
+func parseDataAndSave(url string, db *gorm.DB) error {
+	// Mengirim permintaan GET ke URL JSON
+	response, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
 
+	// Membaca isi respon sebagai byte
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+
+	// Membuat variabel untuk menyimpan respons JSON
+	var jsonResponse Response
+
+	// Unmarshal byte menjadi struct Response
+	err = json.Unmarshal(body, &jsonResponse)
+	if err != nil {
+		return err
+	}
+
+	// Periksa apakah ada data untuk disimpan
+	if len(jsonResponse.Data) == 0 {
+		return nil
+	}
+
+	// Iterasi setiap data dan lakukan proses insert ke database
+	for _, userData := range jsonResponse.Data {
+		user := Customer{
+			Email:     userData.Email,
+			FirstName: userData.FirstName,
+			LastName:  userData.LastName,
+			Avatar:    userData.Avatar,
+		}
+		db.Create(&user)
+	}
+
+	return nil
+}
 func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{db}
 }
